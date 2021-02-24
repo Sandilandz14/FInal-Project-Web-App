96fca91018769bb2e19fe13ffd552b465811c936
Sandilandz14 <stukani100@gmail.com>
2021-02-24 16:29:56 +0200

    Added all updated files

---
+++ root.js
@@ -0,0 +1,18 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// PASSWORD
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
