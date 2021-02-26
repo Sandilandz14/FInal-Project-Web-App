8576239ae064c8bee88cc6e065746ac1e0c19468
Sandilandz14 <stukani100@gmail.com>
2021-02-26 16:13:59 +0200

    Updated filess added

--- root.js
+++ root.js
@@ -1,3 +1,25 @@
+function addUsers() {
+  const inputs = document.getElementsByTagName("input");
+
+  fetch("http://127.0.0.1:5000/add-data/", {
+    method: "POST",
+    body: JSON.stringify({
+      name: inputs[0].value,
+      email: inputs[1].value,
+      addr: inputs[2].value,
+      password: inputs[3].value,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      alert("User has been created");
+      document.getElementById("reg-form").reset();
+    });
+}
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
