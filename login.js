a9fc2d9bae7e1cf538fa468b37191486e6f78f7d
Sandilandz14 <stukani100@gmail.com>
2021-03-05 12:15:53 +0200

    Added updated files

--- login.js
+++ login.js
@@ -1,20 +1,20 @@
-// const loginText = document.querySelector(".title-text .login");
-// const loginForm = document.querySelector("form.login");
-// const loginBtn = document.querySelector("label.login");
-// const signupBtn = document.querySelector("label.signup");
-// const signupLink = document.querySelector("form .signup-link a");
-// signupBtn.onclick = () => {
-//   loginForm.style.marginLeft = "-50%";
-//   loginText.style.marginLeft = "-50%";
-// };
-// loginBtn.onclick = () => {
-//   loginForm.style.marginLeft = "0%";
-//   loginText.style.marginLeft = "0%";
-// };
-// signupLink.onclick = () => {
-//   signupBtn.click();
-//   return false;
-// };
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = () => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+};
+loginBtn.onclick = () => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+};
+signupLink.onclick = () => {
+  signupBtn.click();
+  return false;
+};
 
 let users;
 
@@ -26,15 +26,15 @@ fetch("http://127.0.0.1:5000/show-records/")
   });
 
 function login() {
-  let inputs = document.getElementsByTagName("input");
+  const form = document.getElementById("login");
+  const inputs = form.getElementsByTagName("input");
 
-  let username = inputs[0].value;
-  let password = inputs[1].value;
-
+  let eml = inputs[0].value;
+  let pswrd = inputs[1].value;
+  console.log(eml);
+  console.log(pswrd);
   let loggedIn = users.filter((user) => {
-    return user.username == username && user.password == password
-      ? true
-      : false;
+    return user.email == eml && user.password == pswrd ? true : false;
   });
 
   if (loggedIn.length > 0) {
@@ -54,3 +54,11 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
