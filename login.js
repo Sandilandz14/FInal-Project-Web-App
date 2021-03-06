785ac4e8e9899af2b4deecc1cc8b660ee03bab61
Sandilandz14 <stukani100@gmail.com>
2021-03-06 13:59:19 +0200

    Updated files

--- login.js
+++ login.js
@@ -8,22 +8,22 @@ fetch("http://127.0.0.1:5000/show-records/")
   });
 
 function login() {
-  const form = document.getElementById("login");
-  let inputs = form.getElementsByTagName("input");
+  // const form = document.getElementById("login");
+  let inputs = document.getElementsByTagName("input");
 
   let eml = inputs[0].value;
   let pswrd = inputs[1].value;
   console.log(eml);
   console.log(pswrd);
   let loggedIn = users.filter((user) => {
-    return user.email == eml && user.password == pswrd;
+    return user.email == eml && user.password == pswrd ? true : false;
   });
 
   console.log(loggedIn);
 
   if (loggedIn.length > 0) {
     alert("You are now logged in");
-    window.location.href = "/index.html";
+    window.location.href = "index.html";
   } else {
     alert("Credentials invalid");
   }
